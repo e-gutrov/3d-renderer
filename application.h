9155aa7d173d2c56aae27499b84486eb27f9b6b9
Egor Gutrov <gutrov.egor@gmail.com>
2021-04-25 17:32:32 +0300

    Refactored to Application class.

---
+++ application.h
@@ -0,0 +1,36 @@
+//
+// Created by egor on 25.04.2021.
+//
+
+#ifndef INC_3D_RENDERER_APPLICATION_H
+#define INC_3D_RENDERER_APPLICATION_H
+
+#include "src/screen.h"
+#include "src/world.h"
+#include "src/renderer.h"
+#include "src/camera.h"
+#include <SFML/Graphics.hpp>
+
+class Application {
+public:
+    Application(int screenWidth, int screenHeight);
+    void Run();
+    void AddObject(const Triangle4d& triangle);
+
+private:
+    void Draw();
+    void ProcessEvent(const sf::Event& event);
+    void ProcessKeyPressedEvent(const sf::Event::KeyEvent& keyEvent);
+
+    Screen Screen_;
+    World World_;
+    Renderer Renderer_;
+    Camera Camera_;
+
+    sf::RenderWindow Window_;
+    sf::Texture Texture_;
+    sf::Sprite Sprite_;
+
+};
+
+#endif //INC_3D_RENDERER_APPLICATION_H
--- test.cpp
+++ test.cpp
@@ -1,135 +1,63 @@
-#include <iostream>
-#include <memory>
-#include <SFML/Graphics.hpp>
-#include <Geometry>
+#include "application.h"
 
-#include "src/renderer.h"
-
-int main() {
-    const int W = 640, H = 480;
-    World world;
-
-    // testing projective
-    world.AddObject(
+void PrepareWorld(Application* app) {
+    app->AddObject(
             Triangle4d(
                     Eigen::Vector4d(5, 0, -5, 1),
                     Eigen::Vector4d(5, 2, -5, 1),
                     Eigen::Vector4d(7, 2, -5, 1),
-                    {200, 0, 0, 255}
-    ));
-    world.AddObject(
-    Triangle4d(
-            Eigen::Vector4d(7, 0, -5, 1),
-            Eigen::Vector4d(7, 2, -5, 1),
-            Eigen::Vector4d(5, 0, -5, 1),
-            {200, 0, 0, 255}
-    ));
+                    {200, 0, 0, 255}));
+    app->AddObject(
+            Triangle4d(
+                    Eigen::Vector4d(7, 0, -5, 1),
+                    Eigen::Vector4d(7, 2, -5, 1),
+                    Eigen::Vector4d(5, 0, -5, 1),
+                    {200, 0, 0, 255}));
 
-    world.AddObject(
+    app->AddObject(
             Triangle4d(
                     Eigen::Vector4d(5, 0, -7, 1),
                     Eigen::Vector4d(5, 2, -7, 1),
                     Eigen::Vector4d(7, 2, -7, 1),
-                    {200, 200, 0, 255}
-            ));
-    world.AddObject(
+                    {200, 200, 0, 255}));
+    app->AddObject(
             Triangle4d(
                     Eigen::Vector4d(7, 0, -7, 1),
                     Eigen::Vector4d(7, 2, -7, 1),
                     Eigen::Vector4d(5, 0, -7, 1),
-                    {200, 200, 0, 255}
-            ));
+                    {200, 200, 0, 255}));
 
 
-    world.AddObject(
+    app->AddObject(
             Triangle4d(
                     Eigen::Vector4d(5, 0, -5, 1),
                     Eigen::Vector4d(5, 2, -5, 1),
                     Eigen::Vector4d(5, 0, -7, 1),
-                    {0, 0, 200, 255}
-            ));
-    world.AddObject(
+                    {0, 0, 200, 255}));
+    app->AddObject(
             Triangle4d(
                     Eigen::Vector4d(5, 0, -7, 1),
                     Eigen::Vector4d(5, 2, -7, 1),
                     Eigen::Vector4d(5, 2, -5, 1),
-                    {0, 0, 200, 255}
-            ));
+                    {0, 0, 200, 255}));
 
-    world.AddObject(
+    app->AddObject(
             Triangle4d(
                     Eigen::Vector4d(7, 0, -5, 1),
                     Eigen::Vector4d(7, 2, -5, 1),
                     Eigen::Vector4d(7, 0, -7, 1),
-                    {0, 200, 0, 255}
-            ));
-    world.AddObject(
+                    {0, 200, 0, 255}));
+    app->AddObject(
             Triangle4d(
                     Eigen::Vector4d(7, 0, -7, 1),
                     Eigen::Vector4d(7, 2, -7, 1),
                     Eigen::Vector4d(7, 2, -5, 1),
-                    {0, 200, 0, 255}
-            ));
+                    {0, 200, 0, 255}));
+}
 
-    Renderer r(&world);
-    Camera cam;
-    cam.Shift(Eigen::Vector3d(6, 1, 0));
-    Screen screen(W, H);
-    r.Render(cam, &screen);
-    sf::RenderWindow window(sf::VideoMode(screen.GetWidth(), screen.GetHeight()), "Test");
-    sf::Clock clock;
-    float lastTime = 0;
-    while (window.isOpen()) {
-        r.Render(cam, &screen);
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            switch (event.type) {
-                case sf::Event::Closed:
-                    window.close();
-                    break;
-                case sf::Event::KeyPressed:
-                    switch (event.key.code) {
-                        case sf::Keyboard::A:
-                            cam.Shift(Eigen::Vector3d(-0.1, 0, 0));
-                            break;
-                        case sf::Keyboard::D:
-                            cam.Shift(Eigen::Vector3d(0.1, 0, 0));
-                            break;
-                        case sf::Keyboard::W:
-                            cam.Shift(Eigen::Vector3d(0, 0, -0.1));
-                            break;
-                        case sf::Keyboard::S:
-                            cam.Shift(Eigen::Vector3d(0, 0, 0.1));
-                            break;
-                        case sf::Keyboard::Up:
-                            cam.Rotate(Eigen::Vector3d(0.1, 0, 0));
-                            break;
-                        case sf::Keyboard::Down:
-                            cam.Rotate(Eigen::Vector3d(-0.1, 0, 0));
-                            break;
-                        case sf::Keyboard::Left:
-                            cam.Rotate(Eigen::Vector3d(0, -0.1, 0));
-                            break;
-                        case sf::Keyboard::Right:
-                            cam.Rotate(Eigen::Vector3d(0, 0.1, 0));
-                            break;
-                    }
-            }
-        }
-
-        sf::Texture texture;
-        texture.create(screen.GetWidth(), screen.GetHeight());
-
-        sf::Sprite sprite(texture);
-        texture.update((sf::Uint8*)screen.GetColors().data());
-        window.draw(sprite);
-
-        window.display();
-        float currentTime = clock.getElapsedTime().asSeconds();
-        float fps = 1.f / (currentTime - lastTime);
-        std::cout << fps << '\n';
-        lastTime = currentTime;
-    }
+int main() {
+    Application app(640, 480);
+    PrepareWorld(&app);
+    app.Run();
     return 0;
 }
